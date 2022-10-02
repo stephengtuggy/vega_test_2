6ec36acbdb173f0bc9b56979739fb009ba10e95c
Stephen G Tuggy <sgt@stephengtuggy.com>
2022-10-02 15:43:54 -0700

    *.cpp, *.hpp: Add copyleft

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+// Copyright (c) 2022 Stephen G. Tuggy
+//
+// This file is part of Vega Test 2.
+//
+// Vega Test 2 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Vega Test 2 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Vega Test 2. If not, see <https://www.gnu.org/licenses/>.
+
 #include <iostream>
 #include <boost/smart_ptr/intrusive_ptr.hpp>
 //#include <boost/smart_ptr/make_shared.hpp>
