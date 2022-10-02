d2eb0e5230b078fd20d88dc1c1acba7f8e32ef3f
Stephen G Tuggy <sgt@stephengtuggy.com>
2022-10-02 15:25:31 -0700

    main.cpp: Add top-level unit to a flightgroup

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 //#include <boost/smart_ptr/make_shared.hpp>
 //#include <boost/smart_ptr/shared_ptr.hpp>
 #include "unit.hpp"
+#include "flightgroup.hpp"
 
 boost::intrusive_ptr<VegaTest2::Unit> theTopLevelUnit() {
     static boost::intrusive_ptr<VegaTest2::Unit> kTopLevelUnit = new VegaTest2::Unit();
@@ -11,6 +12,11 @@ boost::intrusive_ptr<VegaTest2::Unit> theTopLevelUnit() {
 
 int main() {
     std::cout << "Top Level Unit: " << theTopLevelUnit()->getFgID() << std::endl;
+
+    VegaTest2::Flightgroup * flightgroup = new VegaTest2::Flightgroup("Shlimazel");
+    flightgroup->setLeader(theTopLevelUnit());
+
+    std::cout << "Top Level Unit after adding to Flightgroup: " << theTopLevelUnit()->getFgID() << std::endl;
 
     return 0;
 }
